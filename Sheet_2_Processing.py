2180f7d92ce65212de2cd8b79af98da801e69b7a
ngdghnam <129106189+ngdghnam@users.noreply.github.com>
2024-01-22 13:52:13 +0700

    Update Sheet_2_Processing.py

--- Sheet_2_Processing.py
+++ Sheet_2_Processing.py
@@ -25,7 +25,26 @@ total_popped_elements_list = turn_series_to_list(df, 'total')
 date_popped_elements_list = turn_series_to_list(df, 'date')
 film_popped_elements_list = turn_series_to_list(df, 'film')
 slot_popped_elements_list = turn_series_to_list(df, 'slot type')
-popcorn_popped_elements_lists = turn_series_to_list(df, 'popcorn')
+popcorn_popped_elements_list = turn_series_to_list(df, 'popcorn')
+customer_popped_list = turn_series_to_list(df, 'customerid')
+time_popped_list = turn_series_to_list(df, 'time')
 
-df2['Popcorn_Price'] = None 
-df2['Popcorn_Price'] = df2['popcorn'].apply(lambda x:0 if x == 'Không' else None)
+# turn them into array 
+total_popped_arr = np.array(total_popped_elements_list)
+date_popped_arr = np.array(date_popped_elements_list)
+film_popped_arr = np.array(film_popped_elements_list)
+slot_popped_arr = np.array(slot_popped_elements_list)
+popcorn_popped_arr = np.array(popcorn_popped_elements_list)
+customer_popped_arr = np.array(customer_popped_list)
+time_popped_arr = np.array(time_popped_list)
+
+for i in range(len(date_popped_arr)):
+    if customer_popped_arr[i] == customer_popped_arr[i+1] and date_popped_arr[i] == date_popped_arr[i+1] and time_popped_arr[i] == time_popped_arr[i+1] and film_popped_arr[i] == film_popped_arr[i+1]:  
+        if popcorn_popped_arr[i] == 'Không':
+            pass 
+        elif popcorn_popped_arr[i] == 'Có':
+            pass
+        else:
+            pass
+        pass 
+
